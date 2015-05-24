5e4abbb5af9740dcfc46ffbbd7961098c02c7b10
yxjxx <mynameisyxjxx@gmail.com>
2015-05-24 09:37:43 +0800

    Initial Commit

---
+++ EBoxPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EBoxPro
+//
+//  Created by yxj on 5/24/15.
+//  Copyright (c) 2015 yxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
