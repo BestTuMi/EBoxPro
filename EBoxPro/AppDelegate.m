fdc0ff6a1d338d0a78e325fcdb46e468c357890e
yxjxx <mynameisyxjxx@gmail.com>
2015-05-24 09:41:53 +0800

    delete storyboard related files

--- EBoxPro/AppDelegate.m
+++ EBoxPro/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    UINavigationController * rootNavigationController = [[UINavigationController alloc] init];
+    [rootNavigationController pushViewController:[[ViewController alloc] init] animated:NO];
+    self.window.rootViewController = rootNavigationController;
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
