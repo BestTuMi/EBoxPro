55bafedc1d539c21f5db5e87c13dc80e6e16aad8
yxjxx <mynameisyxjxx@gmail.com>
2015-05-24 09:51:49 +0800

    add LoginViewController Class

---
+++ EBoxPro/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  EBoxPro
+//
+//  Created by yxj on 5/24/15.
+//  Copyright (c) 2015 yxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
