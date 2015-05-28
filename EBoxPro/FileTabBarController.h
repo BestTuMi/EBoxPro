0fb3e46ff0374bb622cec48d4d3f095d5d25f3aa
yxjxx <mynameisyxjxx@gmail.com>
2015-05-28 16:11:23 +0800

    add Class FileTabBarController (login success destination)

---
+++ EBoxPro/FileTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  FileTabBarController.h
+//  EBoxPro
+//
+//  Created by yxj on 5/28/15.
+//  Copyright (c) 2015 yxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FileTabBarController : UITabBarController
+
+@end
